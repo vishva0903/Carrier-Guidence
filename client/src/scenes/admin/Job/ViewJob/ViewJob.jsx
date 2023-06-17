f4f40a11a7da2a87c122ac72e06f9e5a8dfdf042
adharshSciPy <adharsh.scipy@gmail.com>
2023-06-17 16:43:55 +0530

    admin get client and server done

--- client/src/scenes/admin/Job/ViewJob/ViewJob.jsx
+++ client/src/scenes/admin/Job/ViewJob/ViewJob.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import './viewjob.css'
+import { DataGrid } from "@mui/x-data-grid";
+import axios from 'axios'
+import uuid from "react-uuid"
 
 function ViewJob() {
+
+  const [rows, setRows] = React.useState({});
+  async function setRow() {
+    await axios
+      .get(`http://localhost:2000/job/getJOB`)
+      .then((res) => {
+        setRows(res.data.result);
+        console.log(res.data.result);
+
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+  React.useEffect(() => {
+    setRow();
+  }, []);
+
+
+  const columns = [
+    { field: "_id", headerName: "Id", width: 300, hideable: false, hide: true },
+    { field: "jobTitle", headerName: "jobTitle", width: 300 },
+    { field: "companyName", headerName: "companyName", width: 350 },
+    { field: "jobRequirements", headerName: "jobRequirements", width: 300 },
+  ]
+
+
   return (
-    <div>ViewJob</div>
+    <div className='admin-viewjob'>
+      <div className="admin-table">
+        <DataGrid rows={rows} columns={columns} getRowId={(row: any) => uuid()} />
+      </div>
+    </div>
   )
 }
 
--- server/server.js
+++ server/server.js
@@ -1,29 +1,31 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const dotenv=require('dotenv')
-const app= express();
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+const cors = require('cors')
+const app = express();
 dotenv.config()
-const port=process.env.PORT;
+app.use(cors())
+const port = process.env.PORT;
 const bodyParser = require('body-parser')
-const CollegeRouter=require('./router/collegerouter')
-const FeedbackRouter=require('./router/feebackrouter')
-const JobRouter=require('./router/jobrouter');
+const CollegeRouter = require('./router/collegerouter')
+const FeedbackRouter = require('./router/feebackrouter')
+const JobRouter = require('./router/jobrouter');
 const User = require('./router/user');
 app.use(bodyParser.json())
 
-app.use('/college',CollegeRouter)
-app.use('/feedback',FeedbackRouter)
-app.use('/job',JobRouter)
-app.use('/',User)
-app.listen(port,()=>{
+app.use('/college', CollegeRouter)
+app.use('/feedback', FeedbackRouter)
+app.use('/job', JobRouter)
+app.use('/', User)
+app.listen(port, () => {
     console.log(`app listening to port ${port}`)
 })
 
 
 
-mongoose.connect(process.env.MONGODB,{
+mongoose.connect(process.env.MONGODB, {
 })
-.then(()=>console.log("mongoose connected"))
-.catch((err)=>console.log("error"))
+    .then(() => console.log("mongoose connected"))
+    .catch((err) => console.log("error"))
 
 
